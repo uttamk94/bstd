39e89d5abc9810acda7ed02112363be7da653e48
uttam <uttam_cuet@gmail.com>
2026-04-13 22:49:14 +0600

    updated

--- bstd/src/dev_sett/inc/dev_sett.h
+++ bstd/src/dev_sett/inc/dev_sett.h
@@ -1,4 +1,33 @@
 #pragma once
 
+
+
+typedef enum {
+    DS_CHG_EVNT,
+    DS_BAT_LVL,
+    DS_TMP_EVNT,
+    DS_HL_EVNT,
+    DS_MAX
+} ds_cmd_t;
+
+typedef struct {
+    char chg;
+    char level;
+    char tmp;
+    char hl;
+} ds_data_t;
+
+typedef struct {
+    void (*chg_cb)(char chg);
+    void (*level_cb)(char level);
+    void (*tmp_cb)(char tmp);
+    void (*hl_cb)(char hl);
+} ds_listner_t;
+
+int insert_dev_sett(ds_cmd_t cmd, unsigned int len, void *data);
+ds_data_t get_ds_data();
+
+int set_ds_listner(ds_listner_t *listner);
+
 int init_dev_sett();
 int start_dev_sett();
--- bstd/src/dev_sett/src/dev_sett.c
+++ bstd/src/dev_sett/src/dev_sett.c
@@ -1,5 +1,76 @@
-#include "dev_sett.h"
+#include <zephyr/kernel.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "dev_sett.h"
+
+#define MAX_LISTNER 0x10
+
+typedef struct {
+    ds_cmd_t cmd;
+    unsigned int len;
+    void *data;
+} ds_msg_t;
+
+K_MSGQ_DEFINE(ds_msgq, sizeof(ds_msg_t), 16, 4);
+
+ds_listner_t *listeners[MAX_LISTNER];
+ds_data_t ds_data = {0, };
+
+ds_data_t get_ds_data() {
+    return ds_data;
+}
+
+void set_ds_data(ds_data_t data) {
+    ds_data = data;
+}
+
+int insert_dev_sett(ds_cmd_t cmd, unsigned int len, void *data) {
+    ds_msg_t msg_copy = {cmd, len, data};
+    return k_msgq_put(&ds_msgq, &msg_copy, K_NO_WAIT);
+}
+
+int set_ds_listner(ds_listner_t *listner) {
+    for (int i = 0; i < MAX_LISTNER; i++) {
+        if (!listeners[i]) {
+            listeners[i] = listner;
+            return 0;
+        }
+    }
+    return -1;
+}
+
+void ds_chg_handler(unsigned int len, void *data) {
+    printf("%s: %d\n", __func__, len);
+    unsigned char *chg = (unsigned char *) data;
+    for (int i = 0; i < MAX_LISTNER; i++) {
+        if (listeners[i] && listeners[i]->chg_cb) {
+            listeners[i]->chg_cb(*chg);
+        }
+    }
+    free(chg);
+    chg = NULL;
+}
+
+void ds_task_cb(void *p1, void *p2, void *p3) {
+    printf("%s: started\n", __func__);
+    ds_msg_t msg;
+    while (1) {
+        if (!k_msgq_get(&ds_msgq, &msg, K_FOREVER)) {
+            switch (msg.cmd)
+            {
+            case DS_CHG_EVNT:
+                ds_chg_handler(msg.len, msg.data);
+                break;
+            
+            default:
+                break;
+            }
+        }
+    }
+}
+
+K_THREAD_DEFINE(ds_task, 1024, ds_task_cb, NULL, NULL, NULL, 12, 0, 0);
 
 int init_dev_sett() {
     printf("init_dev_sett\n");
